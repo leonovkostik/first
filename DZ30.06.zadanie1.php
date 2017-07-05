91c5c07022ed7d672030f01e069c1b009831366b
leonovkostik <leonov_kostik@ukr.net>
2017-07-05 22:47:54 +0300

    DZ

---
+++ DZ30.06.zadanie1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 18.06.2017
+ * Time: 15:04;
+ */;
+ $begin = strtotime(25-02-2010);
+ $end = strtotime(22-02-2014);
+ echo 'Виктор Янукович был президентом'.($end - $begin).'секунд.';
+?>
