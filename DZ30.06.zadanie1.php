8892ee00e930d5869f84ff597faac05f3f6edd6a
leonovkostik <leonov_kostik@ukr.net>
2017-07-05 23:45:21 +0300

    DZ

--- DZ30.06.zadanie1.php
+++ DZ30.06.zadanie1.php
@@ -3,9 +3,15 @@
  * Created by PhpStorm.
  * User: Admin
  * Date: 18.06.2017
- * Time: 15:04;
- */;
- $begin = strtotime(25-02-2010);
- $end = strtotime(22-02-2014);
- echo 'Виктор Янукович был президентом'.($end - $begin).'секунд.';
+ * Time: 15:04
+ */
+date_default_timezone_set('Europe/Kiev');
+$newyear = strtotime('2018-01-01');
+$seychas = time();
+$endsummer = strtotime('2017-09-01');
+$nextmonday = strtotime('next monday');
+echo 'Сейчас '.date('Y-m-d H-i-s'),'<br/>';
+echo 'До нового 2018 года осталось:     '.(($newyear - $seychas)/60)." минут",'<br/>',"\n";
+echo  'До конца лета осталось:  '.($endsummer - $seychas)."секунд",'<br/>',"\n";
+echo 'До следующего понедельника осталось:  '.(($nextmonday - $seychas)/3600).'часов',"\n";
 ?>
